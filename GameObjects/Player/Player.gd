39b1aedf3a999cff451dee08d17101e36067f64f
Jon Chapman <jonkchpmn@gmail.com>
2024-05-21 21:03:20 -0400

    Player work and initial missile setup

--- GameObjects/Player/Player.gd
+++ GameObjects/Player/Player.gd
@@ -1,22 +1,32 @@
 extends CharacterBody2D
 
+# Spellpoint vars
+@onready var spell_anim_player = $PlayerSpellPoint/SpellPointSprite/SpellPointAnimPlayer
+var spell_blast_active = false
+
+# Consts
 const SPEED = 135
 const ROTATE_SPEED = 100
-var anim_dir = "L"
-var anim_mode = "Idle"
+
+# Player vars
+var player_anim_dir = "l"
+var player_anim_mode = "idle"
 var index
 
 
-func init(index):
-	self.index = index
+func init(_index):
+	self.index = _index
 	self.set_name("Player" + str(index))
 	get_node("PlayerSprite").self_modulate = GameManager.player_dicts[index][0]
 
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	MovementLoop()
 	AimLoop()
+
+func _process(_delta):
 	AnimationLoop()
+	SpellAnimationLoop()
 
 
 func MovementLoop():
@@ -38,8 +48,21 @@ func MovementLoop():
 func AimLoop():
 	var aim_dir = Input.get_vector("aim_left" + str(index), "aim_right" + str(index), "aim_up" + str(index), "aim_down" + str(index))
 	if aim_dir != Vector2.ZERO:
-		$PlayerSpellPoint.rotation = lerp_angle($PlayerSpellPoint.rotation, aim_dir.angle(), 0.5)
+		$PlayerSpellPoint.rotation = aim_dir.angle()
+		#lerp_angle($PlayerSpellPoint.rotation, aim_dir.angle(), 0.5)
 
 
 func AnimationLoop():
 	pass
+
+func SpellAnimationLoop():
+	var aim_dir = Input.get_vector("aim_left" + str(index), "aim_right" + str(index), "aim_up" + str(index), "aim_down" + str(index))
+	var spell_animation = "inactive"
+	
+	if spell_blast_active:
+		spell_animation = "blast"
+	elif aim_dir != Vector2.ZERO && !spell_blast_active:
+		spell_animation = "cast"
+		
+	spell_anim_player.play(spell_animation)
+
